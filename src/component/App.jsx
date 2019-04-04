d9aa283999af1a848a2371849f7c707b2f070392
ArtemBaskal <asbaskal@edu.hse.ru>
2019-04-05 01:50:10 +0300

    Proprs in CardsContainer deleted

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar />
-        <CardsContainer universe={this.props.name}/>
+        <CardsContainer />
         <Universe name="dc" />
         <Universe name="marvel" />
       </div>
