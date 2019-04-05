31f65e8687d5c6edbaad4e7cf595eb7179fd80f7
ArtemBaskal <asbaskal@edu.hse.ru>
2019-04-06 01:34:55 +0300

    Update App.jsx

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -3,7 +3,6 @@ import json from "../json/superheroes";
 import CardContainer from "./CardContainer";
 import SearchBar from "./SearchBar";
 import Universe from "./Universe";
-import Test from "./test";
 
 class App extends React.Component {
   render() {
